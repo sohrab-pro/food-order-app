e01ae6edc6fdde8f10c4be95c6e3ac1e1ff33447
Sohrab Khan <122888702+sohrabmvsol@users.noreply.github.com>
2023-09-08 20:02:01 +0300

    style for cart items added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,19 +2,35 @@ import React, { useContext } from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
 import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
 	const cartCtx = useContext(CartContext);
 	const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+
+	const hasItems = cartCtx.items.length > 0;
+	const cartItemRemoveHandler = (id) => {
+		//
+	};
+
+	const cartItemAddHandler = (item) => {
+		//
+	};
+
 	const cartItems = (
 		<ul className={classes["cart-items"]}>
 			{cartCtx.items.map((item) => (
-				<li key={item.id}>{item.name}</li>
+				<CartItem
+					key={item.id}
+					name={item.name}
+					amount={item.amount}
+					price={item.price}
+					onRemove={cartItemRemoveHandler.bind(null, item.id)}
+					onAdd={cartItemAddHandler.bind(null, item)}
+				/>
 			))}
 		</ul>
 	);
-
-	const hasItems = cartCtx.items.length > 0;
 
 	return (
 		<Modal onClose={props.onClose}>
